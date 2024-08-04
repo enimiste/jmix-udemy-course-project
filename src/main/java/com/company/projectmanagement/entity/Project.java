555d7c2b34517f68da5695f64e3a28372b8475e6
Enimiste <nouni.elbachir@gmail.com>
2024-08-05 00:56:05 +0200

    F : create Project Entity

---
+++ src/main/java/com/company/projectmanagement/entity/Project.java
@@ -0,0 +1,66 @@
+package com.company.projectmanagement.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "PROJECT", indexes = {
+        @Index(name = "IDX_PROJECT_MANAGER", columnList = "MANAGER_ID")
+})
+@Entity
+public class Project {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @InstanceName
+    @Column(name = "NAME", nullable = false)
+    @NotNull
+    private String name;
+
+    @JoinColumn(name = "MANAGER_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User manager;
+
+    @Column(name = "DESCRIPTION")
+    @Lob
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getManager() {
+        return manager;
+    }
+
+    public void setManager(User manager) {
+        this.manager = manager;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
