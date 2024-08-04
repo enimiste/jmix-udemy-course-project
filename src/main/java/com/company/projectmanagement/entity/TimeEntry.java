b77ad38a0a70439b3906fd8bdbce8ea7462773f2
Enimiste <nouni.elbachir@gmail.com>
2024-08-05 01:15:09 +0200

    F : add TimeEntry entity

---
+++ src/main/java/com/company/projectmanagement/entity/TimeEntry.java
@@ -0,0 +1,94 @@
+package com.company.projectmanagement.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "TIME_ENTRY", indexes = {
+        @Index(name = "IDX_TIME_ENTRY_TASK", columnList = "TASK_ID"),
+        @Index(name = "IDX_TIME_ENTRY_USER", columnList = "USER_ID")
+})
+@Entity
+public class TimeEntry {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @JoinColumn(name = "TASK_ID", nullable = false)
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Task task;
+
+    @Column(name = "TIME_SPENT", nullable = false)
+    @NotNull
+    private Integer timeSpent;
+
+    @Column(name = "ENTRY_DATE", nullable = false)
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    private Date entryDate;
+
+    @JoinColumn(name = "USER_ID", nullable = false)
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private User user;
+
+    @InstanceName
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(Date entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public Integer getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(Integer timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
