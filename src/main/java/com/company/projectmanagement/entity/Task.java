5abe24c8b2fbb293adb3e7a3bf087b51c9673fb6
Enimiste <nouni.elbachir@gmail.com>
2024-08-05 01:10:35 +0200

    F : add Task entity

---
+++ src/main/java/com/company/projectmanagement/entity/Task.java
@@ -0,0 +1,101 @@
+package com.company.projectmanagement.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "TASK_", indexes = {
+        @Index(name = "IDX_TASK__ASSIGNEE", columnList = "ASSIGNEE_ID")
+})
+@Entity(name = "Task_")
+public class Task {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @InstanceName
+    @Column(name = "NAME", nullable = false)
+    @NotNull
+    private String name;
+
+    @Column(name = "DUE_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date dueDate;
+
+    @JoinColumn(name = "ASSIGNEE_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User assignee;
+
+    @Column(name = "PRIORITY")
+    private String priority;
+
+    @Column(name = "DESCRIPTION")
+    @Lob
+    private String description;
+
+    @Column(name = "ESTIMATION")
+    private Integer estimation;
+
+    public Integer getEstimation() {
+        return estimation;
+    }
+
+    public void setEstimation(Integer estimation) {
+        this.estimation = estimation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskPriority getPriority() {
+        return priority == null ? null : TaskPriority.fromId(priority);
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority == null ? null : priority.getId();
+    }
+
+    public User getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(User assignee) {
+        this.assignee = assignee;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
