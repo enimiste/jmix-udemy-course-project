2834048ed49ae11789d8988dfe7ae8fcd4189d12
Enimiste <nouni.elbachir@gmail.com>
2024-08-07 01:25:16 +0200

    F : set default priority before editing Task entity using a handler

--- src/main/java/com/company/projectmanagement/view/task/TaskDetailView.java
+++ src/main/java/com/company/projectmanagement/view/task/TaskDetailView.java
@@ -1,16 +1,19 @@
 package com.company.projectmanagement.view.task;
 
 import com.company.projectmanagement.entity.Task;
+import com.company.projectmanagement.entity.TaskPriority;
 import com.company.projectmanagement.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.view.*;
 
 @Route(value = "tasks/:id", layout = MainView.class)
 @ViewController("Task_.detail")
 @ViewDescriptor("task-detail-view.xml")
 @EditedEntityContainer("taskDc")
 public class TaskDetailView extends StandardDetailView<Task> {
+    @Subscribe
+    public void onInitEntity(final InitEntityEvent<Task> event) {
+        Task entity = event.getEntity();
+        entity.setPriority(TaskPriority.MEDUIM);
+    }
 }
