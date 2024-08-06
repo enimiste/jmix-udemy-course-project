8e5d3400fc94354079392ea45f9c187cf6928d7c
Enimiste <nouni.elbachir@gmail.com>
2024-08-06 17:25:20 +0200

    F : implement task detail view from scratch

---
+++ src/main/java/com/company/projectmanagement/view/task/TaskDetailView.java
@@ -0,0 +1,18 @@
+package com.company.projectmanagement.view.task;
+
+
+import com.company.projectmanagement.entity.Task;
+import com.company.projectmanagement.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.model.InstanceContainer;
+import io.jmix.flowui.model.InstanceLoader;
+import io.jmix.flowui.view.*;
+
+@Route(value = "tasks/:id", layout = MainView.class)
+@ViewController("Task_.detail")
+@ViewDescriptor("task-detail-view.xml")
+@EditedEntityContainer("taskDc")
+@DialogMode(width = "64em")
+public class TaskDetailView extends StandardDetailView<Task> {
+
+}
