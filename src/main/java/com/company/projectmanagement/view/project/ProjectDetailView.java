d7b969060e658caab67a8d7e0eac2cba4f35d8c9
Enimiste <nouni.elbachir@gmail.com>
2024-08-06 15:12:43 +0200

    F : add DialogMode width to the ProjectDetailView

--- src/main/java/com/company/projectmanagement/view/project/ProjectDetailView.java
+++ src/main/java/com/company/projectmanagement/view/project/ProjectDetailView.java
@@ -3,14 +3,12 @@ package com.company.projectmanagement.view.project;
 import com.company.projectmanagement.entity.Project;
 import com.company.projectmanagement.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.view.*;
 
 @Route(value = "projects/:id", layout = MainView.class)
 @ViewController("Project.detail")
 @ViewDescriptor("project-detail-view.xml")
 @EditedEntityContainer("projectDc")
+@DialogMode(width = "64em")
 public class ProjectDetailView extends StandardDetailView<Project> {
 }
