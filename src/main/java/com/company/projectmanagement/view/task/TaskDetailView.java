91df3c7c15758ad198453aabd68c34f6d205f379
Enimiste <nouni.elbachir@gmail.com>
2024-08-07 01:04:16 +0200

    F : Generate Task views using Studio

---
+++ src/main/java/com/company/projectmanagement/view/task/TaskDetailView.java
@@ -0,0 +1,16 @@
+package com.company.projectmanagement.view.task;
+
+import com.company.projectmanagement.entity.Task;
+import com.company.projectmanagement.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.EditedEntityContainer;
+import io.jmix.flowui.view.StandardDetailView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@Route(value = "tasks/:id", layout = MainView.class)
+@ViewController("Task_.detail")
+@ViewDescriptor("task-detail-view.xml")
+@EditedEntityContainer("taskDc")
+public class TaskDetailView extends StandardDetailView<Task> {
+}
