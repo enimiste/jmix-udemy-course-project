a58b4597c2014de78651865e99165963eceef17e
Enimiste <nouni.elbachir@gmail.com>
2024-08-07 10:55:12 +0200

    F : generate a JPQL query using code snippets

--- src/main/java/com/company/projectmanagement/app/NewBean.java
+++ src/main/java/com/company/projectmanagement/app/NewBean.java
@@ -1,10 +1,15 @@
 package com.company.projectmanagement.app;
 
 import com.company.projectmanagement.entity.Project;
+import com.company.projectmanagement.entity.Task;
 import io.jmix.core.DataManager;
 import io.jmix.core.FetchPlan;
 import io.jmix.core.querycondition.PropertyCondition;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 public class NewBean {
@@ -12,15 +17,14 @@ public class NewBean {
 
     public NewBean(DataManager dataManager) {
         this.dataManager = dataManager;
+    }
 
-        System.out.println(
-                dataManager.load(Project.class)
-                        .condition(PropertyCondition.contains(
-                                "name", "USG"
-                        ))
-                        .maxResults(2)
-                        .fetchPlan(FetchPlan.BASE)
-                        .list()
-        );
+    public List<Task> loadTasksByQuery(){
+        return dataManager.load(Task.class)
+                .query("select t from Task_ t " +
+                        "where extract(month from t.dueDate) = :month " +
+                        "order by t.name asc")
+                .parameter("month", LocalDate.now().getMonthValue())
+                .list();
     }
 }
