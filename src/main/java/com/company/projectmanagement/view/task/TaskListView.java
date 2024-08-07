2ebecc39d21c5e5823ff48a66a218f7875e95e5c
Enimiste <nouni.elbachir@gmail.com>
2024-08-07 13:04:38 +0200

    F : add new column on Task list view to download attachment

--- src/main/java/com/company/projectmanagement/view/task/TaskListView.java
+++ src/main/java/com/company/projectmanagement/view/task/TaskListView.java
@@ -2,8 +2,14 @@ package com.company.projectmanagement.view.task;
 
 import com.company.projectmanagement.entity.Task;
 import com.company.projectmanagement.view.main.MainView;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.download.Downloader;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Route(value = "tasks", layout = MainView.class)
@@ -12,4 +18,23 @@ import io.jmix.flowui.view.*;
 @LookupComponent("tasksDataGrid")
 @DialogMode(width = "64em")
 public class TaskListView extends StandardListView<Task> {
+    @ViewComponent
+    private DataGrid<Task> tasksDataGrid;
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private Downloader downloader;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        tasksDataGrid.addComponentColumn(task -> {
+            Button button = uiComponents.create(Button.class);
+            button.setIcon(VaadinIcon.DOWNLOAD.create());
+            button.setThemeName("tertiary-inline");
+            button.addClickListener(e -> downloader.download(task.getAttachment()));
+            button.setEnabled(task.getAttachment() != null);
+            return button;
+        });
+    }
+
 }
