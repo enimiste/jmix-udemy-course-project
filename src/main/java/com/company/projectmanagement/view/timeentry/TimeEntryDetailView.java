c865404b732d485ff8d52f8efecb03aca16a0b66
NOUNI EL Bachir <nouni.elbachir@gmail.com>
2024-08-08 02:05:17 +0200

    F : set current auth user as user for new TimeEntry

--- src/main/java/com/company/projectmanagement/view/timeentry/TimeEntryDetailView.java
+++ src/main/java/com/company/projectmanagement/view/timeentry/TimeEntryDetailView.java
@@ -1,9 +1,11 @@
 package com.company.projectmanagement.view.timeentry;
 
 import com.company.projectmanagement.entity.TimeEntry;
+import com.company.projectmanagement.entity.User;
 import com.company.projectmanagement.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.TimeSource;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,12 +18,16 @@ import java.time.ZonedDateTime;
 public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
     @Autowired
     private TimeSource timeSource;
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
 
     @Subscribe
     public void onInitEntity(final InitEntityEvent<TimeEntry> event) {
         final ZonedDateTime zonedDateTime = timeSource.now();
         TimeEntry entity = event.getEntity();
         entity.setEntryDate(zonedDateTime.toLocalDateTime());
+        final User user = (User) currentAuthentication.getUser();
+        entity.setUser(user);
     }
 
 }
