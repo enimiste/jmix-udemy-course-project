1a148cdec0977f9b4a68b0b95a9ab19fa32f971b
Enimiste <nouni.elbachir@gmail.com>
2024-08-09 22:49:06 +0200

    F : create a new Row-level Role to restrict access to projects (Predicate based)

---
+++ src/main/java/com/company/projectmanagement/security/RestrictedProjectsRole.java
@@ -0,0 +1,33 @@
+package com.company.projectmanagement.security;
+
+import com.company.projectmanagement.entity.Project;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.security.model.RowLevelBiPredicate;
+import io.jmix.security.role.annotation.PredicateRowLevelPolicy;
+import io.jmix.security.role.annotation.RowLevelRole;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static io.jmix.security.model.RowLevelPolicyAction.*;
+
+@RowLevelRole(name = "RestrictedProjectsRole", code = RestrictedProjectsRole.CODE)
+public interface RestrictedProjectsRole {
+    String CODE = "restricted-projects-role";
+
+    /**
+     * This policy checks that the connected user :
+     * <ul>
+     *     <li>Can only update/delete projects which are managed by him.</li>
+     *     <li>Can only create a new project where the he is the manager.</li>
+     *     <li>Can't change the manager of a project.</li>
+     * </ul>
+     * @return
+     */
+    @PredicateRowLevelPolicy(entityClass = Project.class, actions = {CREATE, UPDATE, DELETE})
+    default RowLevelBiPredicate<Project, ApplicationContext> projectPredicate() {
+        return (project, applicationContext) -> {
+            UserDetails user = applicationContext.getBean(CurrentAuthentication.class).getUser();
+            return user.equals(project.getManager());
+        };
+    }
+}
