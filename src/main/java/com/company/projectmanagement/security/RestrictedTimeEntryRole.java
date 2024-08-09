9c57e44d3268bc8ee7c34c86194b1b140c6773bd
Enimiste <nouni.elbachir@gmail.com>
2024-08-09 22:57:33 +0200

    F : create a new Row-level Role to restrict access to time entries (JPQL based)

---
+++ src/main/java/com/company/projectmanagement/security/RestrictedTimeEntryRole.java
@@ -0,0 +1,14 @@
+package com.company.projectmanagement.security;
+
+import com.company.projectmanagement.entity.TimeEntry;
+import io.jmix.security.role.annotation.JpqlRowLevelPolicy;
+import io.jmix.security.role.annotation.RowLevelRole;
+
+@RowLevelRole(name = "RestrictedTimeEntryRole", code = RestrictedTimeEntryRole.CODE)
+public interface RestrictedTimeEntryRole {
+    String CODE = "restricted-time-entry-role";
+
+    @JpqlRowLevelPolicy(entityClass = TimeEntry.class,
+            where = "{E}.user.username=:current_user_username or {E}.task.project.manager.username=:current_user_username")
+    void timeEntry();
+}
